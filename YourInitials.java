16d45fbad89af99ddf6e86bdd866d3c1190bacf6
Kenneth Rothstein <skottrod@me.com>
2022-10-08 09:45:29 -0700

    Completed Project

---
+++ YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println("K   K   SSS   RRRR");
+        System.out.println("K  K   S   S  R   R");
+        System.out.println("K K    S      R   R");
+        System.out.println("KK      SSS   RRRR");
+        System.out.println("K K        S  R R");
+        System.out.println("K  K   S   S  R  R");
+        System.out.println("K   K   SSS   R   R");
+    }
+}
